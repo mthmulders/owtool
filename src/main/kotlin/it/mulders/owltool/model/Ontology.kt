957ee4b5366a9c1deb320757569fecf1d8dda48e
Maarten Mulders <m.th.mulders@xs4all.nl>
2025-09-29 09:49:41 +0200

    feat: Count classes in an ontology

--- src/main/kotlin/it/mulders/owltool/model/Ontology.kt
+++ src/main/kotlin/it/mulders/owltool/model/Ontology.kt
@@ -2,4 +2,14 @@ package it.mulders.owltool.model
 
 data class Ontology(
     val classes: Collection<Class>,
-)
+) {
+    fun classCount(): Int {
+        return classes.countRecursively()
+    }
+
+    private fun Collection<Class>.countRecursively(): Int {
+        return this.asSequence()
+            .map { it.children.countRecursively() + 1 }
+            .sum()
+    }
+}
