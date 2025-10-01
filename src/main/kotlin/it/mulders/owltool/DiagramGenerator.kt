0749463f08f9a9d641e335819dca42b10d5652d7
Maarten Mulders <m.th.mulders@xs4all.nl>
2025-10-01 12:17:19 +0200

    feat: Specify namespace for which to generate the diagram

---
+++ src/main/kotlin/it/mulders/owltool/DiagramGenerator.kt
@@ -0,0 +1,7 @@
+package it.mulders.owltool
+
+import java.nio.file.Path
+
+fun interface DiagramGenerator {
+    fun generateDiagram(path: Path, namespace: String): Result<Path>
+}
--- src/main/kotlin/it/mulders/owltool/cli/DiagramCommand.kt
+++ src/main/kotlin/it/mulders/owltool/cli/DiagramCommand.kt
@@ -1,5 +1,6 @@
 package it.mulders.owltool.cli
 
+import it.mulders.owltool.DiagramGenerator
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import picocli.CommandLine
@@ -8,9 +9,9 @@ import kotlin.jvm.java
 @CommandLine.Command(
     name = "diagram",
     mixinStandardHelpOptions = true,
-    description = ["Generate a diagram of the ontology"],
+    description = ["Generate a diagram of the ontology in a namespace"],
 )
-class DiagramCommand : Runnable {
+class DiagramCommand(private val generator: DiagramGenerator) : Runnable {
     @CommandLine.Parameters(
         arity = "1",
         description = ["Path to the ontology source"],
@@ -18,8 +19,20 @@ class DiagramCommand : Runnable {
     )
     lateinit var input: String
 
+    @CommandLine.Parameters(
+        arity = "1",
+        description = ["Namespace for which to generate a diagram"],
+        paramLabel = "namespace",
+    )
+    lateinit var namespace: String
+
     override fun run() {
         log.info("Generating a diagram for $input")
+
+        if (!namespace.endsWith("#")) namespace += "#"
+
+        log.info("Generating diagram; input={}, namespace={}", path, namespace)
+        val result = generator.generateDiagram(path, namespace)
     }
 
     companion object {
